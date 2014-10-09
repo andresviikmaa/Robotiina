5c52999451bd5c2580adcb4b8d6611175aea4d18
martinappo <martinappo@gmail.com>
2014-10-09 19:57:47 +0300

    Rotate/flip frame.

--- camera.cpp
+++ camera.cpp
@@ -26,6 +26,11 @@ const cv::Mat &Camera::Capture()
 {
 	if (cap->isOpened())
 		*cap >> frame;
+		cv::flip(frame, frame, 0);
+		cv::Point2f src_center(frame.cols / 2.0F, frame.rows / 2.0F);
+		cv::Mat rot_matrix = getRotationMatrix2D(src_center, 180.0, 1.0);
+		cv::Mat rotated_img(cv::Size(frame.size().height, frame.size().width), frame.type());
+		warpAffine(frame, frame, rot_matrix, frame.size());
     return frame;
 }
 const cv::Mat &Camera::CaptureHSV() {
