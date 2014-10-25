3776ed8cb26ca2ab14f2a1602ce5904b6b052962
martinappo <martinappo@gmail.com>
2014-10-25 14:47:46 +0300

    Coilboard conf reading from conf file (conf file update needed)

--- coilBoard.h
+++ coilBoard.h
@@ -4,6 +4,8 @@
 #include <boost/thread/thread.hpp>
 #include <boost/atomic.hpp>
 #include <boost/timer/timer.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 
 class CoilBoard : SimpleSerial
 {
@@ -15,7 +17,11 @@ private:
 	boost::posix_time::ptime waitTime = time;
 	boost::posix_time::time_duration waitDuration;
 public:
-	CoilBoard(boost::asio::io_service &io_service, std::string port = "port", unsigned int baud_rate = 115200) : SimpleSerial(io_service, port, baud_rate) {
+	CoilBoard(boost::asio::io_service &io_service, std::string port, unsigned int baud_rate = 115200) : SimpleSerial(io_service, port, baud_rate) {
+		using boost::property_tree::ptree;
+		ptree pt;
+		read_ini("conf/ports.ini", pt);
+		port = pt.get<std::string>(std::to_string(ID_COILGUN));
 		stop_thread = false;
 		threads.create_thread(boost::bind(&CoilBoard::Ping, this));
 	};
